ff2a8da072d0ef0a49e0e5095705b4a3f40e5a29
iuk1net <carlos_iuka88@hotmail.com>
2026-03-05 12:51:39 +0100

    fix: restaurar SyntiaMvpApplication.java perdido durante git operations

---
+++ src/main/java/com/syntia/mvp/SyntiaMvpApplication.java
@@ -0,0 +1,16 @@
+package com.syntia.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal de la aplicación Syntia MVP.
+ */
+@SpringBootApplication
+public class SyntiaMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyntiaMvpApplication.class, args);
+    }
+}
+
